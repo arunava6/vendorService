5fef3928e6b9ee7b9c3cafc0e9d4687fe3e3b062
jaya <admin@admin.com>
2026-06-18 20:29:55 +0530

    Update src/main/java/com/training/controller/VendorController.java

--- src/main/java/com/training/controller/VendorController.java
+++ src/main/java/com/training/controller/VendorController.java
@@ -8,14 +8,6 @@ public class VendorController {
 
     @GetMapping("/")
     public String index() {
-        return "Welcome to Spring Boot Vendor Service API!"\n"
-             + "-----------------------------------------\n"
-             + "Status   : RUNNING\n"
-             + "Version  : 1.0.0\n"
-             + "Module   : Vendor Management System\n"
-             + "Database : MySQL Connected\n"
-             + "API      : /vendor/controller/getVendors\n"
-             + "-----------------------------------------\n"
-             + "Built with Spring Boot!";
+        return "Welcome to Spring Boot Vendor Service API!";
     }
 }
